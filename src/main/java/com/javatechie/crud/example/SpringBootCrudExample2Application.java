f03890f247de7311e782484714d5272bcf6bc87c
Vaishnvee-Shinde <vaishnvee169@gmail.com>
2022-05-27 13:22:13 +0530

    first commit

---
+++ src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -0,0 +1,15 @@
+package com.javatechie.crud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class SpringBootCrudExample2Application {
+
+	public static void main(String[] args) {
+		run(SpringBootCrudExample2Application.class, args);
+	}
+
+}
